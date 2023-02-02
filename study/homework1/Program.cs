24954e21e8665009c2779450837c961f5e393332
Sergey <sesmbile1@gmail.com>
2023-02-02 18:29:21 +0200

    проба

--- study/homework1/Program.cs
+++ study/homework1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine(num1);
 int num2 = new Random().Next(1,101); //eeeee
 Console.WriteLine(num2);
 int summ = num1 + num2;
-Console.Write("сумма чисел= ");
+Console.Write("сумма чисеלל= ");
 Console.Write(summ);
