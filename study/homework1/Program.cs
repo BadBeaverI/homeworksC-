3d3d974534e33ed5f04d0abb6424a4fc2d87d462
Sergey <sesmbile1@gmail.com>
2023-02-02 19:15:01 +0200

    домашка 1я задача

--- study/homework1/Program.cs
+++ study/homework1/Program.cs
@@ -1,7 +1,21 @@
-﻿int num1 = new Random().Next(1,101); //eeeee
-Console.WriteLine(num1);
-int num2 = new Random().Next(1,101); //eeeee
-Console.WriteLine(num2);
-int summ = num1 + num2;
-Console.Write("сумма чисеלל= ");
-Console.Write(summ);
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+
+
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.Write("MAX=");
+    Console.Write(a);
+else 
+    Console.Write("MAX=");
+    Console.Write(b);
